62df99c2e521311940dc74cc4dc82f13ce113e3a
Michael Heyman <mitch.heyman@gmail.com>
2017-04-28 20:51:35 -0700

    difficulty 1

--- LLL/list.cpp
+++ LLL/list.cpp
@@ -2,6 +2,31 @@
 
 //Write a function to display the list. Return the number of nodes.   
 
+void list::test() {
+    cout << "\nUser-defined function calls:\n\n";
+
+    cout << "displayAll():" << endl;
+    cout << "This list has " << this->displayAll() << " items.\n\n";
+
+    cout << "displayReverse():" << endl;
+    cout << "\nThis list has " << this->displayReverse() << " items.\n\n";
+
+    cout << "displayDivisible(2):" << endl;
+    cout << "\nThis list has " << this->displayDivisible(2) << " items.\n\n";
+    
+    cout << "displayEveryOther():" << endl;
+    cout << "\nThis list hid " << this->displayEveryOther() << " items.\n\n";
+
+    cout << "addToEnd():" << endl;
+    cout << "This list has a sum of " << this->addToEnd() << " items.\n\n";
+    
+    cout << "addAfter2(300):" << endl;
+    cout << "This function added " << this->addAfter2(300) << " nodes to the list.\n\n";
+
+    cout << "addBefore2(500):" << endl;
+    cout << "This function removed " << this->addBefore2(500) << " nodes to the list.\n\n";
+}
+
 int list::displayAll() {
     return displayAll(head);
 }
@@ -61,4 +86,102 @@ int list::displayDivisibleHelper(node* head, int toDisplay, int count) {
     }
 
     return displayDivisibleHelper(head->next, toDisplay, count);
+}
+
+//Write a function to display every other Node in the list. Return the number of nodes that are not displayed.
+int list::displayEveryOther() {
+    return displayEveryOther(head);
+}
+int list::displayEveryOther(node* head) {
+    int count = 0;
+    int displayed = 0;
+    return displayEveryOtherHelper(head, count, displayed);
+}
+int list::displayEveryOtherHelper(node* head, int &count, int &displayed) {
+    if (!head)
+        return count - displayed;
+
+    if (count % 2 == 0) {
+        cout << head->data << " ";
+        displayed++;
+    }
+
+    return displayEveryOtherHelper(head->next, ++count, displayed);
+}
+
+//Write a function to add the sum of the data from the first two nodes to the end of the list. Return the sum of the list.    
+int list::addToEnd() {
+    return addToEnd(head);
+}
+int list::addToEnd(node*& head) {
+    int index = 0;
+    int sum = 0;
+    int total = 0;
+    return addToEndHelper(head, index, sum, total);
+}
+int list::addToEndHelper(node*& head, int &index, int &sum, int &total) {
+    if (!head)
+        return total;
+
+    if (!head->next) {
+        head->data += sum;
+        //return total;
+    }
+
+    if (index == 0 || index == 1)
+        sum += head->data;
+
+    total += head->data;
+
+    return addToEndHelper(head->next, ++index, sum, total);
+}
+
+//Write a function to add the data passed in to the function after every instance of a 2 in the list. Return the number of nodes that were added to the list. 
+int list::addAfter2(int toAdd) {
+    return addAfter2(head, toAdd);
+}
+int list::addAfter2(node*& head, int toAdd) {
+    return addAfter2Helper(head, toAdd, 0);
+}
+int list::addAfter2Helper(node*& head, int toAdd, int added) {
+    if (!head)
+        return added;
+
+    if (head->data == 2) {
+        node * newNode = new node;
+        newNode->data = toAdd;
+        newNode->next = head->next;
+        head->next = newNode;
+        added++;
+    }
+
+    return addAfter2Helper(head->next, toAdd, added);
+}
+
+//Write a function to add the data passed in to the function before every instance of a 2 in the list. Return the number of nodes that were added to the list.    
+int list::addBefore2(int toAdd) {
+    return addBefore2(head, toAdd);
+}
+int list::addBefore2(node*& head, int toAdd) {
+    return addBefore2Helper(head, toAdd, 0);
+}
+int list::addBefore2Helper(node*& head, int toAdd, int added) {
+    if (!head)
+        return added;
+
+    if (head->next) {
+        if (head->next->data == 2) {
+            node * newNode = new node;
+            newNode->data = toAdd;
+            newNode->next = head->next;
+            head->next = newNode;
+            added++;
+            return addBefore2Helper(newNode->next, toAdd, added);
+        }
+    }
+
+    return addBefore2Helper(head->next, toAdd, added);
+
+    //rework this method, and keep track of previous node to handle the case where
+    //the head is 2
 }
--- LLL/list.h
+++ LLL/list.h
@@ -22,6 +22,7 @@ class list
 
         /* *****************YOUR TURN! ******************************** */
         //Write your function prototype here:
+        void test();
 
         //Write a function to display the list. Return the number of nodes.   
         int displayAll();
@@ -37,6 +38,35 @@ class list
         int displayDivisible(int toDisplay);
         int displayDivisible(node* head, int toDisplay);
         int displayDivisibleHelper(node* head, int toDisplay, int count);
+
+        //Write a function to display every other Node in the list. Return the number of nodes that are not displayed.
+        int displayEveryOther();
+        int displayEveryOther(node* head);
+        int displayEveryOtherHelper(node* head, int &count, int &displayed);
+
+        //Write a function to add the sum of the data from the first two nodes to the end of the list. Return the sum of the list.    
+        int addToEnd();
+        int addToEnd(node*& head);
+        int addToEndHelper(node*& head, int &index, int &sum, int &total);
+
+        //Write a function to add the data passed in to the function after every instance of a 2 in the list. Return the number of nodes that were added to the list. 
+        int addAfter2(int toAdd);
+        int addAfter2(node*& head, int toAdd);
+        int addAfter2Helper(node*& head, int toAdd, int added);
+
+        //Write a function to add the data passed in to the function before every instance of a 2 in the list. Return the number of nodes that were added to the list.    
+        int addBefore2(int toAdd);
+        int addBefore2(node*& head, int toAdd);
+        int addBefore2Helper(node*& head, int toAdd, int added);
+
+        /*
+           3  Write a function to add the data of the first node that is divisible by three or five to every following node in the list. Return number of nodes that occur before one is divisible by three or five.  int modifyAfter(node* head)
+           3  Write a function to reverse the List. Return number of nodes in the list.   int reverseAll(node*& head)
+           3  Write a function to add a node that contains the data passed into the function at the end of the list for each node that is divisible by the first node's data  int appendIfDivisible (node*& head, int toAdd)
+           4  Write a function that takes 2 lists and creates a new list where the nth node is the average of the nth items in the two input lists. Return the average of the new list.   float CopySpecialAvg (node*& source1, node*& source2, node*& dest)
+           3  Write a function that returns the average of the unique items in the list.  float averageSpecial (node* head)
+           4  Write a function that reverses every 3 nodes in a list, any remainders at the end of the list should be left as they are. Return the number of swapped nodes    int swap3(node*& head)
+           */
 
     private:		//notice there is both a head and a tail!
         node * head;

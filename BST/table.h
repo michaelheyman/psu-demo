034d8735b829a391486b5d2159501a7a2951d80d
Michael Heyman <mitch.heyman@gmail.com>
2017-07-21 21:18:53 -0700

    BST init

--- BST/table.h
+++ BST/table.h
@@ -22,6 +22,20 @@ class table
 
         /* ************** PLACE YOUR PROTOTYPE HERE ***************** */
 
+        /*
+        0   Write a function to display the tree in order. Return the number of nodes.  int displayAll(node* root)
+        0   Write a function to display the tree in reverse order. Return the number of nodes.  int displayReverse(node* root)
+        0   Write a function to display every node that is divisible by the argument passed in. Return number of nodes displayed.   int displayDivisible(node* root, int toDisplay)
+        1   Write a function to add the data passed in to the function to the tree. int insertData(node*& root, int toAdd)
+        3   Write a function to remove every node that is divisible by 3 and 5 from the Tree. Return the number of nodes removed    int removeDivisible(node*& root)
+        3   Write a function to display the data in the longest path of the tree.   int longestPath(node* root)
+        4   Write a function to return the average of the longest path in a tree.   int avgPath(node* root)
+        3   Write a function that returns the average of the data contained in the tree int avgData(node* root)
+        2   Write a function to copy all the nodes that are greater than root into a new tree. Return the number of nodes copied    int copyGreater(node* srcRoot, node*& destRoot)
+        3   Write a function to copy all even nodes from one tree into a new tree. return number of nodes divisible by 7    int copyDivisible(node* srcRoot, node*& destRoot)
+        2   Write a function to remove all the leaves from a tree. return number of nodes removed.  int prune(node*& root)
+        3   Write a function to copy a tree, excluding its' root, into a new tree. return number of nodes copied.   int copyNoRoot(node* srcRoot, node*& destRoot)
+        */
     private:
         node * root;
 };

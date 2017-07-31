8e112a9b675781c4408186c127605eaa1eedf987
Michael Heyman <mitch.heyman@gmail.com>
2017-07-31 12:18:11 -0700

    LLL displayAll

--- LLL/list.cpp
+++ LLL/list.cpp
@@ -1,1 +1,15 @@
 #include "list.h"
+
+//Write a function to display the list. Return the number of nodes.
+int list::displayAll() {
+    cout << __func__ << endl;
+    return displayAll(head);
+}
+int list::displayAll(node* head) {
+    if (!head)
+        return 0;
+
+    cout << head->data << " ";
+
+    return 1 + displayAll(head->next);
+}

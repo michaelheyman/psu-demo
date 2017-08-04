75eb591b2ce2e41e4efbc5c2c50e3d36089f22fb
Michael Heyman <mitch.heyman@gmail.com>
2017-08-03 18:07:05 -0700

    CLL displayAll

--- CLL/clist.cpp
+++ CLL/clist.cpp
@@ -1,1 +1,19 @@
 #include "clist.h"
+
+//Write a function to display the list. Return the number of nodes.
+int list::displayAll() {
+    cout << __func__ << endl;
+    return displayAll(rear->next);
+}
+int list::displayAll(node* head) {
+    if (!head)
+        return 0;
+    if (head == this->rear) {
+        cout << head->data << " ";
+        return 1;
+    }
+
+    cout << head->data << " ";
+
+    return 1 + displayAll(head->next);
+}

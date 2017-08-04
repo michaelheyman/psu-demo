44218aa00dbba3050b28b41a1772196697393e46
Michael Heyman <mitch.heyman@gmail.com>
2017-08-03 18:10:28 -0700

    CLL displayReverse

--- CLL/clist.cpp
+++ CLL/clist.cpp
@@ -17,3 +17,22 @@ int list::displayAll(node* head) {
 
     return 1 + displayAll(head->next);
 }
+
+//Write a function to display the list in reverse order. Return the number of nodes.
+int list::displayReverse() {
+    cout << __func__ << endl;
+    return displayReverse(rear->next);
+}
+int list::displayReverse(node* head) {
+    if (!head)
+        return 0;
+    if (head == rear) {
+        cout << head->data << " ";
+        return 1;
+    }
+
+    int count = 1 + displayReverse(head->next);
+    cout << head->data << " ";
+
+    return count;
+}
